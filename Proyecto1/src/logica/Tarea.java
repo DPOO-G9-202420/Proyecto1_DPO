d07865825a89d942755fb637550d41d1304f562d
Maria Clara Aponte <mc.aponte2@uniandes.edu.co>
2024-10-22 12:38:26 -0500

    tarea

--- Proyecto1/src/logica/Tarea.java
+++ Proyecto1/src/logica/Tarea.java
@@ -1,9 +1,17 @@
 package logica;
 
 import java.util.ArrayList;
+import logica.Actividad;
 
 public class Tarea extends Actividad{
 	private String metodoEntrega;
+	private String estado;
+
+	public Tarea(String metodoEntrega, String estado) {
+		//super();
+		this.metodoEntrega=metodoEntrega;
+		this.estado=estado;
+	}
 	
 	
 
