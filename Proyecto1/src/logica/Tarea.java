ff52bca0a4100e1f668cc946a86940679cf53979
Maria Clara Aponte <mc.aponte2@uniandes.edu.co>
2024-10-22 23:24:30 -0500

    tarea(no esta completada)

--- Proyecto1/src/logica/Tarea.java
+++ Proyecto1/src/logica/Tarea.java
@@ -7,8 +7,8 @@ public class Tarea extends Actividad{
 	private String metodoEntrega;
 	private String estado;
 
-	public Tarea(String metodoEntrega, String estado) {
-		//super();
+	public Tarea(String metodoEntrega, String estado, String titulo, String descripcion, int dificultad, int duracion) {
+		super();
 		this.metodoEntrega=metodoEntrega;
 		this.estado=estado;
 	}
