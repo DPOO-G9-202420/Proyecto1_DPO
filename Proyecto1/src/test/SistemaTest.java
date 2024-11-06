483f838e499552cf80041e5721280768abbb2677
Maria Clara Aponte <mc.aponte2@uniandes.edu.co>
2024-11-06 17:33:51 -0500

    sistematest1

---
+++ Proyecto1/src/test/SistemaTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Duration;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import logica.Sistema;
+
+public class SistemaTest {
+	private Sistema sistema;
+	
+	
+	
+	
+}
