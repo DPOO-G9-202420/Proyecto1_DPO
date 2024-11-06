fa88f43e71cfa57f0bc6a00a7befb6f20d7ff8cf
Maria Clara Aponte <mc.aponte2@uniandes.edu.co>
2024-11-06 18:10:28 -0500

    test

--- Proyecto1/src/test/SistemaTest.java
+++ Proyecto1/src/test/SistemaTest.java
@@ -13,11 +13,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import logica.Sistema;
+import logica.Estudiante;
+import logica.Usuario;
+import logica.Profesor;
 
 public class SistemaTest {
 	private Sistema sistema;
+	private Usuario usuario;
+	private Estudiante estudiante;
+	private Profesor profesor;
 	
+	@BeforeEach
+	public void SetUp() {
+		this.sistema=new Sistema();
+		estudiante= new Estudiante("mc.aponte", "1234");
+		profesor=new Profesor ("d.vergara112", "dani2004");
+	}
 	
+	@Test
+	public void getTipoUsuarioTest() {
+
+	}
 	
 	
 }
